9bd5d369979ed4ee934b8f6fdbc70c36802bb0bd
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-12 14:30:26 +0300

    Bağlantı Dizesi

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<RepositoryContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"),
+        b => b.MigrationsAssembly("WebApi"));
+});
 
 var app = builder.Build();
 
