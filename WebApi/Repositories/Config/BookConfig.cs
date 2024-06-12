bb9f93485a012fc72f4aa6a0717330f13e0f49ab
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-12 14:51:08 +0300

    Entities

--- WebApi/Repositories/Config/BookConfig.cs
+++ WebApi/Repositories/Config/BookConfig.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore;
-using WebApi.Models;
+using Entities.Models;
 
 namespace WebApi.Repositories.Config
 {
