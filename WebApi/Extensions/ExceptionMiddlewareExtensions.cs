e2dd5eb48d17497055a10d1b4e5680e2251975b8
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-12 19:32:23 +0300

    Use Exception Handler daha iyi düzeltildi

--- WebApi/Extensions/ExceptionMiddlewareExtensions.cs
+++ WebApi/Extensions/ExceptionMiddlewareExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using Entities.ErrorModel;
+using Entities.Exceptions;
 using Microsoft.AspNetCore.Diagnostics;
 using Services.Contracts;
-using System.Net;
 
 namespace WebApi.Extensions
 {
@@ -10,21 +10,26 @@ namespace WebApi.Extensions
         public static void ConfigureExceptionHandler(this WebApplication app,
             ILoggerService logger)
         {
-            app.UseExceptionHandler(appErr =>
+            app.UseExceptionHandler(appError =>
             {
-                appErr.Run(async context =>
+                appError.Run(async context =>
                 {
-                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
                     context.Response.ContentType = "application/json";
-
                     var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
                     if (contextFeature is not null)
                     {
-                        logger.LogError($"Someting went wrong:{contextFeature.Error}");
+                        context.Response.StatusCode = contextFeature.Error switch
+                        {
+                            NotFoundException => StatusCodes.Status404NotFound,
+                            _ => StatusCodes.Status500InternalServerError
+                        };
+
+                        logger.LogError($"Something went wrong: {contextFeature.Error}");
+
                         await context.Response.WriteAsync(new ErrorDetails()
                         {
                             StatusCode = context.Response.StatusCode,
-                            Message = "Internal Server Error"
+                            Message = contextFeature.Error.Message
                         }.ToString());
                     }
                 });
