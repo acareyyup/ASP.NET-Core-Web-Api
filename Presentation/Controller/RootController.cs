072613081bb60d07e6169c2a1db0724be75be690
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-19 10:36:59 +0300

    Root Controller

---
+++ Presentation/Controller/RootController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace Presentation.Controller
+{
+    [ApiController]
+    [Route("api")]
+    public class RootController : ControllerBase
+    {
+        private readonly LinkGenerator _linkGenerator;
+
+        public RootController(LinkGenerator linkGenerator)
+        {
+            _linkGenerator = linkGenerator;
+        }
+
+
+        public async Task<IActionResult> GetRoot([FromHeader(Name = "Accept")] string mediaType)
+        {
+            if (mediaType.Contains("application/vnd.ASPNETCOREAPI.apiroot"))
+            {
+
+            }
+
+
+            return NoContent(); // 204
+        }
+
+
+
+
+
+    }
+}
