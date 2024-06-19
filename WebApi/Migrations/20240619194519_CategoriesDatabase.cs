9e2f4dccc7c0c09c73fd5a7f04c8c8c652c0520f
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-19 22:45:50 +0300

    Category Config ve Seed Data

---
+++ WebApi/Migrations/20240619194519_CategoriesDatabase.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoriesDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "531fa71e-ba04-4eb5-ada6-1dddd4f8f6f6");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5ec39c18-51f5-4842-9dfb-3da495c81e97");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "69143801-c4b0-4722-a916-25c154e8c70b");
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "3b6b3eec-9fad-4bfd-80a3-34eed1d8f480", null, "User", "USER" },
+                    { "9087bb7f-4094-4791-9032-86ca0e11a01d", null, "Editor", "EDITOR" },
+                    { "bec64383-b748-4afe-81ec-af1e261bc540", null, "Admin", "ADMIN" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[,]
+                {
+                    { 1, "Computer Science" },
+                    { 2, "Network" },
+                    { 3, "Database Management Systems" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3b6b3eec-9fad-4bfd-80a3-34eed1d8f480");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "9087bb7f-4094-4791-9032-86ca0e11a01d");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "bec64383-b748-4afe-81ec-af1e261bc540");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "531fa71e-ba04-4eb5-ada6-1dddd4f8f6f6", null, "User", "USER" },
+                    { "5ec39c18-51f5-4842-9dfb-3da495c81e97", null, "Editor", "EDITOR" },
+                    { "69143801-c4b0-4722-a916-25c154e8c70b", null, "Admin", "ADMIN" }
+                });
+        }
+    }
+}
