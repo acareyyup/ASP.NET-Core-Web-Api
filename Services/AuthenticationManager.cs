2d9ed95222a00ea28c784ad7216f80f85e035d97
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-19 15:37:43 +0300

    CreateToken

--- Services/AuthenticationManager.cs
+++ Services/AuthenticationManager.cs
@@ -3,10 +3,14 @@ using Entities.DataTransferObjects;
 using Entities.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Services.Contracts;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -31,6 +35,15 @@ namespace Services
             _configuration = configuration;
         }
 
+        public async Task<string> CreateToken()
+        {
+            var signinCredentials = GetSiginCredentials();
+            var claims = await GetClaims();
+            var tokenOptions = GenerateTokenOptions(signinCredentials, claims);
+            return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+
+        }
+
         public async Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistrationDto)
         {
             var user = _mapper.Map<User>(userForRegistrationDto);
@@ -52,6 +65,46 @@ namespace Services
                 _logger.LogWarning($"{nameof(ValidateUser)} : Authentication failed. Wrong username or password.");
             }
             return result;
+        }
+
+        private SigningCredentials GetSiginCredentials()
+        {
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+            var key = Encoding.UTF8.GetBytes(jwtSettings["secretKey"]);
+            var secret = new SymmetricSecurityKey(key);
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+
+        private async Task<List<Claim>> GetClaims()
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, _user.UserName)
+            };
+
+            var roles = await _userManager
+                .GetRolesAsync(_user);
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            return claims;
+        }
+
+        private JwtSecurityToken GenerateTokenOptions(SigningCredentials signinCredentials,
+            List<Claim> claims)
+        {
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+
+            var tokenOptions = new JwtSecurityToken(
+                    issuer: jwtSettings["validIssuer"],
+                    audience: jwtSettings["validAudience"],
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(Convert.ToDouble(jwtSettings["expires"])),
+                    signingCredentials: signinCredentials);
+
+            return tokenOptions;
         }
     }
 }
