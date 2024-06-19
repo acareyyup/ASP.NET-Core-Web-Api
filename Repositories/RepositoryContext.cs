8e4b5ad5d77d41a05235de106ac3a4f3ecf7a819
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-19 22:37:48 +0300

    DbSet Tanımlama

--- Repositories/RepositoryContext.cs
+++ Repositories/RepositoryContext.cs
@@ -14,6 +14,7 @@ namespace Repositories
 
         }
         public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
