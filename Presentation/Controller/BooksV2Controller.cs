333985d4ab164c517f158831b05dbca8e3c15213
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-19 10:57:01 +0300

    Books V2 Controller

---
+++ Presentation/Controller/BooksV2Controller.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controller
+{
+    [ApiVersion("2.0")]
+    [ApiController]
+    [Route("api/books")]
+    public class BooksV2Controller : ControllerBase
+    {
+    }
+}
