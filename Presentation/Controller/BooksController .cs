b09e58e0f9e716b4e2ac73db6cdc9c25fb00932f
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-19 10:25:45 +0300

    Options

--- Presentation/Controller/BooksController .cs
+++ Presentation/Controller/BooksController .cs
@@ -99,5 +99,14 @@ namespace Presentation.Controllers
 
             return NoContent(); // 204
         }
+
+        [HttpOptions]
+        public IActionResult GetBooksOptions()
+        {
+            Response.Headers.Add("Allow", "GET, PUT, POST, PATCH, DELETE, HEAD, OPTIONS");
+            return Ok();
+        }
+
+
     }
 }
